fed3ea64406241bc656a59c4f4f2de0c6d20bf3f
Kevin Le Scornet <kle-scor@c1r2p7.42nice.fr>
2026-02-19 18:44:07 +0100

    norminette bench count

--- Bench_Codes/bench_count.c
+++ Bench_Codes/bench_count.c
@@ -14,8 +14,8 @@
 
 float	disorder_calculator(t_stack_library *lst)
 {
-	t_stack *searcher;
-	float i;
+	t_stack	*searcher;
+	float	i;
 
 	i = 0;
 	searcher = lst -> begin;
@@ -36,38 +36,40 @@ void	bench_strategy(t_stack_library *stack_a, t_bench *bench, char **argv)
 
 	x = 0;
 	if (inst_detector_bench(argv, bench, x) == 0 && bench->bench_true == 1)
+	{
 		if (bench->disorder < 0.2)
 			ft_print_error("Strategy : Adaptive / O(n2)");
 		else if (bench->disorder >= 0.2 && bench->disorder <= 0.5)
 			ft_print_error("Strategy : Adaptive / O(n√n)");
 		else if (bench->disorder > 0.5)
 			ft_print_error("Strategy : Adaptive / O(n log n)");
+	}
 	else if (inst_detector_bench(argv, bench, x) == 1
-				&& bench->bench_true == 1)
+		&& bench->bench_true == 1)
 		ft_print_error("Strategy : Simple / O(n2)");
 	else if (inst_detector_bench(argv, bench, x) == 2
-				&& bench->bench_true == 1)
+		&& bench->bench_true == 1)
 		ft_print_error("Strategy : Medium / O(n√n)");
 	else if (inst_detector_bench(argv, bench, x) == 3
-				&& bench->bench_true == 1)
+		&& bench->bench_true == 1)
 		ft_print_error("Strategy : Complex / O(n log n)");
 	bench = bench_calculator_error(stack_a, bench);
 }
 
 t_bench	*bench_calculator(t_stack_library *stack_a, t_bench *bench)
 {
-	bench->total_ops = bench->nb_sa + bench->nb_sb + bench->nb_ss +
-	bench->nb_ra + bench->nb_rb + bench->nb_rr + bench->nb_rra +
-	bench->nb_rrb + bench->nb_rrr;
+	bench->total_ops = bench->nb_sa + bench->nb_sb + bench->nb_ss
+		+ bench->nb_ra + bench->nb_rb + bench->nb_rr + bench->nb_rra
+		+ bench->nb_rrb + bench->nb_rrr;
 	bench->dis_percent = bench->disorder * 10000;
 	ft_print_error("Disorder level : %d.%d\n", bench->dis_percent / 100,
-				bench->dis_percent % 100);
+		bench->dis_percent % 100);
 	ft_print_error("Total number of operations : %d\n", bench->total_ops);
 	ft_print_error("sa : %d\tsb : %d\tss : %d\tpa : %d\tpb : %d\n",
 		bench->nb_sa, bench->nb_sb, bench->nb_ss, bench->nb_pa, bench->nb_pb);
 	ft_print_error("ra : %d\trb : %d\trr : %d\t",
-		bench->nb_ra,  bench->nb_rb,  bench->nb_rr);
+		bench->nb_ra, bench->nb_rb, bench->nb_rr);
 	ft_print_error("ra : %d\trb : %d\trr : %d\t",
-		bench->nb_rra,  bench->nb_rrb,  bench->nb_rrr);
+		bench->nb_rra, bench->nb_rrb, bench->nb_rrr);
 	return (bench);
 }
