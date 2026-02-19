a74de59a360e29a1ba632b570b93c95008a22333
Kevin Le Scornet <kle-scor@c1r2p7.42nice.fr>
2026-02-19 19:55:51 +0100

    normalise

--- Fonctions_Kevin/stack_normalizer.c
+++ Fonctions_Kevin/stack_normalizer.c
@@ -12,19 +12,18 @@
 
 #include "../push_swap.h"
 
-// "int total" n'est pas utilisé ici, donc je le retire et on le rajoutera de nouveau si besoin
-void stack_normalizer(t_stack *lst)
+void	stack_normalizer(t_stack *lst)
 {
-	t_stack *marker;
-	t_stack *searcher;
-	int i;
+	t_stack	*marker;
+	t_stack	*searcher;
+	int		i;
 
 	marker = lst;
 	searcher = marker -> next;
 	i = 0;
-	while(marker != NULL)
+	while (marker != NULL)
 	{
-		while(searcher != NULL)
+		while (searcher != NULL)
 		{
 			if (searcher -> content < marker -> content)
 				marker = searcher;
@@ -32,23 +31,24 @@ void stack_normalizer(t_stack *lst)
 		}
 		marker -> order = i;
 		i++;
-		marker = marker -> next; // Remplacé "marker = lst" ?? Why ? La ligne suivante aussi je la comprends pas (Obviously message de Kenny s'il y avait un doute :P)
-		while(marker -> next != NULL && marker -> order != -1)
+		marker = lst;
+		while (marker -> next != NULL && marker -> order != -1)
 			marker = marker -> next;
 		searcher = marker -> next;
 	}
 }
 
-t_stack_library *stack_filler(t_stack_library *stack, int pos, int argc, char **argv)
+t_stack_library	*stack_filler(t_stack_library *stack,
+	int pos, int argc, char **argv)
 {
-	t_stack *writer;
+	t_stack	*writer;
 
 	writer = stack->begin;
-	while(pos < argc)
+	while (pos < argc)
 	{
-	writer -> content = ft_atoi(argv[pos]);
+		writer -> content = ft_atoi(argv[pos]);
 		writer = writer -> next;
-	pos++;
+		pos++;
 	}
 	return (stack);
 }
